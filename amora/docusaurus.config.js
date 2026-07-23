a46992f547fe04202ac2342febbf5b6a68b6c680
Giovane dos Santos Silva <125308043+Giovane2230@users.noreply.github.com>
2026-07-23 21:14:46 +0000

    fixx

--- amora/docusaurus.config.js
+++ amora/docusaurus.config.js
@@ -5,7 +5,6 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
-import CodeBlock from '@theme/CodeBlock';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -56,8 +55,8 @@ const config = {
           
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+      
+  
         },
         theme: {
           customCss: './src/css/custom.css',
